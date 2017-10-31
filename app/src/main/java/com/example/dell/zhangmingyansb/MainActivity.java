0240f72eddd5a6c4e263173621820fdeb8a07133
3115474035 <3115474035@qq.com>
2017-10-31 14:24:04 +0800

    wwwwww

--- app/src/main/java/com/example/dell/zhangmingyansb/MainActivity.java
+++ app/src/main/java/com/example/dell/zhangmingyansb/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //hahahahhaaaaa
+        //高超是个大坏蛋
     }
 }
